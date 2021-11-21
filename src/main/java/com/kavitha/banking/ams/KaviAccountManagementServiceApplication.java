132ec295e1bf087b045ecd0dd63719b5f6a61b1c
Kavitha Reddy Vummadi <kavis60@gmail.com>
2021-11-20 22:16:39 -0500

    Intial project

---
+++ src/main/java/com/kavitha/banking/ams/KaviAccountManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kavitha.banking.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaviAccountManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaviAccountManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kavitha/banking/ams/KaviAccountManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavitha.banking.ams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaviAccountManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
