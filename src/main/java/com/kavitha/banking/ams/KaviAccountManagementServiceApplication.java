63307e56bd41ba3abcd957a809ed9d66e319a7f1
Kavitha Reddy Vummadi <kavis60@gmail.com>
2021-11-20 22:24:57 -0500

    Added commit

--- src/main/java/com/kavitha/banking/ams/KaviAccountManagementServiceApplication.java
+++ src/main/java/com/kavitha/banking/ams/KaviAccountManagementServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KaviAccountManagementServiceApplication {
-
+//-- Test Commit>
 	public static void main(String[] args) {
 		SpringApplication.run(KaviAccountManagementServiceApplication.class, args);
 	}
